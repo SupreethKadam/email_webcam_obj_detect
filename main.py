a0870354c0d84df4cdf4d3d8816918868facae58
Supreeth <supreethkadam@gmail.com>
2023-04-01 12:44:48 +0530

    Capturing frame with object

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import cv2
+import glob
+from emailing import send_email
 
 # Capturing the video using web-camera.
 # Pass the argument 0 if using main camera(integrated camera) or 1 - for external or USB camera.
 video = cv2.VideoCapture(0)
 
 first_frame = None
+status_list = []
+count = 0
 
 while True:
+    status = 0
     check, frame = video.read()
     # convert the color frame to grayscale.
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -29,10 +34,29 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+            # capturing the image frame
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            img_with_obj = all_images[index]
+            print("index: ", index)
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
+
 
     cv2.imshow("My video", frame)
     key = cv2.waitKey(1)
